486c11c4acda11012d36a26b767e6e040565f2f3
Adil Khan <adil.khan@outlook.com>
2017-02-19 21:44:43 -0500

    Moving out content to state

--- components/LinkCard.js
+++ components/LinkCard.js
@@ -2,7 +2,18 @@ import React, { Component } from 'react';
 import { Container, Content, Card, CardItem, Thumbnail, logo, Header, Title,  Button, Left, Right, Body, Icon } from 'native-base';
 import { Text, Image } from 'react-native';
 
+
+function marty() {
+   return "Whoa I asdasdasdas asd ad asd as da sd asd as da sda sd adasdasdasdasd  know javascript";
+}
 export default class LinkCard  extends Component {
+  constructor(props){
+    super();
+    this.state = {
+      url: 'http://www.functionalprog.com',
+      marty: marty()
+    }
+  }
   render() {
     let pic = {
       uri: 'https://upload.wikimedia.org/wikipedia/commons/d/de/Bananavarieties.jpg'
@@ -12,23 +23,17 @@ export default class LinkCard  extends Component {
         <Left>
           <Thumbnail source={pic} />
           <Body>
-            <Text>http://www.cnn.com</Text>
+            <Text>{this.state.url}</Text>
           </Body>
         </Left>
       </CardItem>
       <CardItem cardBody>
-        <Image style={{width: 350, height: 150}} source={pic}/>
+        <Image style={{height: 200, width: 400, alignSelf: 'stretch'}} source={pic}/>
       </CardItem>
       <CardItem content>
-        <Text>Wait a minute. Wait a minute, Doc. Uhhh...
-          Are you telling me that you built a time machine... out of a DeLorean?!
-          Are you telling me that you built a time machine... out of a DeLorean?!
-          Are you telling me that you built a time machine... out of a DeLorean?!
-          Are you telling me that you built a time machine... out of a DeLorean?!
-          Are you telling me that you built a time machine... out of a DeLorean?!
-          Are you telling me that you built a time machine... out of a DeLorean?!
-          Are you telling me that you built a time machine... out of a DeLorean?!
-          Whoa. This is heavy.</Text>
+        <Text>
+        {this.state.marty}
+        </Text>
       </CardItem>
       <CardItem header>
         <Left>
