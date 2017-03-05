c2e233f16585eda915726947c749086115e8299e
Adil Khan <adil.khan@outlook.com>
2017-03-05 13:43:49 -0500

    Uses DataArray Property Of LIst to lazy loa

--- index.ios.js
+++ index.ios.js
@@ -11,9 +11,10 @@ import {
   StyleSheet,
   Text,
   View,
-  Image
+  Image,
+  ListView
 } from 'react-native';
-import { Container,Form, Item, Input, Label, Content, Card, CardItem, Thumbnail, logo, Header, Title,  Button, Left, Right, Body, Icon } from 'native-base';
+import { Container, List,ListItem, Form, Item, Input, Label, Content, Card, CardItem, Thumbnail, logo, Header, Title,  Button, Left, Right, Body, Icon } from 'native-base';
 import LinkCard from './components/LinkCard.js';
 import UrlSearch from './components/UrlSearch.js';
 import DataProvider from './service/DataProvider.js';
@@ -24,19 +25,22 @@ export default class UrlVaultReactNative extends Component {
     let urlData = DataProvider.getUrlData();
     let cards = [ ];
     urlData.urls.forEach((item) => {
-      cards.push(<LinkCard url={item.url} detail={item.content} image={item.image} key={item.url} />)
+      cards.push(<LinkCard url={item.url} detail={item.content} image={item.image} key={item.key} />)
     });
     this.state = {
       cards: cards
     };
     this.myCb = this.myCb.bind(this);
+    this.endReached = this.endReached.bind(this);
     setUvState('searchFunction', this.myCb);
   }
 
   myCb(event) {
     console.log('it worked!:' + getUvState('searchInput'));
   }
-
+  endReached(event) {
+    console.log("End of scroll reached");
+  }
   render() {
     return (
       <Container>
@@ -53,9 +57,48 @@ export default class UrlVaultReactNative extends Component {
         </Header>
         <UrlSearch  />
         <Content>
-          {this.state.cards}
-        </Content>
-      </Container>
+          <List dataArray={DataProvider.getUrlData().urls} renderRow = {(data) =>
+            <ListItem>
+              <Card>
+                <CardItem>
+                  <Left>
+                    <Thumbnail source={{uri: data.image}} />
+                    <Body>
+                      <Text>{data.url}</Text>
+                    </Body>
+                  </Left>
+                </CardItem>
+                <CardItem cardBody>
+                  <Image style={{height: 200, width: 400, alignSelf: 'stretch'}} source={{uri: data.image}}/>
+                </CardItem>
+                <CardItem content>
+                  <Text>
+                    {data.detail}
+                  </Text>
+                </CardItem>
+                <CardItem header>
+                  <Left>
+                    <Button transparent>
+                      <Icon active name="play" />
+                    </Button>
+                  </Left>
+                  <Body style={{alignItems: 'stretch'}}>
+                    <Button transparent>
+                      <Icon active name="md-create" />
+                    </Button>
+                  </Body>
+                  <Right>
+                    <Button transparent>
+                      <Icon active name="md-trash" />
+                    </Button>
+                  </Right>
+                </CardItem>
+              </Card>
+            </ListItem>
+          }>
+        </List>
+      </Content>
+    </Container>
     );
   }
 }
--- service/DataProvider.js
+++ service/DataProvider.js
@@ -1,22 +1,24 @@
 export default class DataProvider {
 
   static getUrlData(val) {
-    let count = 100;
-    let item = {
-      url: 'http://www.cnn.com',
-      content: 'Here is some cool info about cnn.',
-      image: 'https://www.clker.com/cliparts/1/c/7/7/13986776161270179884cute_cat102.png',
-    };
+    let count = 1000;
 
     let data = {
       urls: [
       ]
     };
-    
+
     for (let  i=1; i <= count; i++){
+      let item = {
+        url: 'http://www.cnn.com ' +i ,
+        content: 'Here is some cool info about cnn.',
+        image: 'https://www.clker.com/cliparts/1/c/7/7/13986776161270179884cute_cat102.png',
+        key: i
+      }
       data.urls.push(item);
     }
-     return data;
+    console.log(data);
+    return data;
   }
 
 }
